21c31d7b136e57de8fe95b371d44a078400b510b
NigelYao <nigelyaohang@gmail.com>
2016-01-26 18:28:16 +0800

    initial commit

---
+++ app/src/main/java/com/nigel/luckmoney/wechatoriginalphoto/CheckOriginalPhoto.java
@@ -0,0 +1,35 @@
+package com.nigel.luckmoney.wechatoriginalphoto;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+/**
+ * Created by Nigel on 2016/1/26.
+ */
+public class CheckOriginalPhoto implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(final LoadPackageParam loadPackageParam) throws Throwable {
+
+        if (!loadPackageParam.packageName.contains("tencent.mm")) {
+            return;
+        }
+
+        try {
+
+            findAndHookMethod("com.tencent.mm.plugin.sns.lucky.ui.LuckyRevealImageView", loadPackageParam.classLoader, "getBlurBitmapFilePath", new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    return XposedHelpers.callMethod(param.thisObject,"getOriginBitmapFilePath");
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
